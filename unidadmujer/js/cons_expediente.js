7a80e3b1cde61406c54508f9448b76e1809c1747
erilis <erilis02@gmail.com>
2015-05-13 23:23:19 -0700

    graficas de unidad mujer
    graficas de unidad mujer supuestamente terminadas

--- unidadmujer/js/cons_expediente.js
+++ unidadmujer/js/cons_expediente.js
@@ -10,8 +10,23 @@ $(function(){
 				//convertimos la respuesta del php (jSON) en array de javascript
 				dataJson=eval(responseText);
 			    datos=[];
+
 			    for(var i in dataJson){
+					var si=dataJson[i].label
+					var no=dataJson[i].label
+			    if(si=="t" || no=="f") {		         
+				if(si=="t") {
+					dataJson[i].label="Si";
+datos.push([dataJson[i].label, dataJson[i].value]);
+			}
+			else	if(no=="f") {
+					dataJson[i].label="No";
+datos.push([dataJson[i].label, dataJson[i].value]);
+			}	
+		}
+			else{
 			       datos.push([dataJson[i].label, dataJson[i].value]);
+}
 			    }
 			    //Se crea el titulo del grafico
 			    var titulo="";
@@ -24,6 +39,25 @@ $(function(){
 			     if($("input[name='radBus']:checked").val()=="ne"){
 			    	titulo="Nivel de educacion de las personas con expediente";
 			    }
+			     if($("input[name='radBus']:checked").val()=="ps"){
+			    	titulo="Pasatiempo de las personas con expediente ";
+			    }
+			     if($("input[name='radBus']:checked").val()=="sl"){
+			    	titulo="Situacion laboral ";
+			    }
+			       if($("input[name='radBus']:checked").val()=="de"){
+			    	titulo="Dependencia economica";
+			    }  
+			        if($("input[name='radBus']:checked").val()=="ta"){
+			    	titulo="Tipo de alimentación";
+			    }
+			        if($("input[name='radBus']:checked").val()=="ma"){
+			    	titulo="Estadisticas de maltrato";
+			    }
+			        if($("input[name='radBus']:checked").val()=="as"){
+			    	titulo="Estadisticas de abuso sexual";
+			    }
+			    
 			    muestraGrafica(datos,titulo);
 			}
 		});
